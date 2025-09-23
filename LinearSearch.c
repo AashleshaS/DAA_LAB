e42f8293e7be6959e8f2a3160c31bedbf652593e
AashleshaS <aashlesha1204@gmail.com>
2025-09-23 11:48:37 +0530

    Search the given element with linear search technique using recursion.

---
+++ LinearSearch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void read1(int a[],int n){
+	printf("Enter %d elements : ",n);
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+}
+
+int linearSearch(int a[],int low, int high ,int key){
+	if(low<=high){
+		if(a[low]==key){
+			return low;
+		}
+		low++;
+		return linearSearch(a,low,high,key);
+	}
+	return -1;
+}
+void main() {
+	int a[20], n, pos, key;
+	printf("Enter n value : ");
+	scanf("%d", &n);
+	read1(a, n);
+	printf("Enter a key element : ");
+	scanf("%d", &key);
+	pos = linearSearch(a, 0, n - 1, key);
+	if (pos == -1) {
+		printf("The key element %d is not found\n", key);
+	} else {
+		printf("The key element %d is found at position : %d\n", key, pos);
+	}
+}
